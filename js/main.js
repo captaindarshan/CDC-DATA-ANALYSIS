0e2542767af15d0ed26ec909ac896c87f26b8553
Moburu <stevenschiffner3@gmail.com>
2024-06-03 19:40:02 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
+// Leaflet
 const buildMap = selection => {
 
 }
 
+// Chart.js
 const buildBar = selection => {
 
 }
 
+// Chart.js
 const buildLine = selection => {
 
 }
 
+// Event handler on <select> element
 const handleChange = selection => {
     buildMap(selection);
     buildBar(selection);
